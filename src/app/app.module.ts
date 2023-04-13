53926db090c52f9016cefabd10e24ff511dcaf99
MUHAMMET ÇAĞLAR <1muhammetcaglar@gmail.com>
2023-04-13 03:03:40 +0300

    Angular_reactive_form
    Angular reactive form credit card uygulaması

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardformComponent } from './cardform/cardform.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardformComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
